a639438b691fc8cce8a726a7226c486c4b2b6764
Jarren <jarrentstudent@gmail.com>
2025-11-12 22:13:04 -0700

    Make LED1, 2, 3, 4 blink in second-long intervals

--- app/src/main.c
+++ app/src/main.c
@@ -6,23 +6,37 @@
 #include <zephyr/drivers/gpio.h>
 
 #define LED0_NODE DT_ALIAS(led0)
+#define LED1_NODE DT_ALIAS(led1)
+#define LED2_NODE DT_ALIAS(led2)
+#define LED3_NODE DT_ALIAS(led3)
 
 static const struct gpio_dt_spec led0 = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
+static const struct gpio_dt_spec led1 = GPIO_DT_SPEC_GET(LED1_NODE, gpios);
+static const struct gpio_dt_spec led2 = GPIO_DT_SPEC_GET(LED2_NODE, gpios);
+static const struct gpio_dt_spec led3 = GPIO_DT_SPEC_GET(LED3_NODE, gpios);
 
 int main(void) {
     int ret;
 
-    if(!gpio_is_ready_dt(&led0)) {
-        return -1;
-    }
+    const struct gpio_dt_spec leds[] = {led0, led1, led2, led3};
 
-    ret = gpio_pin_configure_dt(&led0, GPIO_OUTPUT_ACTIVE);
-    if (ret < 0) {
-        return ret;
+    for (int i = 0; i < 4; i++) {
+        if (!gpio_is_ready_dt(&leds[i])) {
+            return -1;
+        }  
+
+        ret = gpio_pin_configure_dt(&leds[i], GPIO_OUTPUT_ACTIVE);
+        if (ret < 0) {
+            return ret;
+        }
     }
+    
+
 
     while (1) {
-        gpio_pin_toggle_dt(&led0);
+        for (int i = 0; i < 4; i++) {
+            gpio_pin_toggle_dt(&leds[i]);
+        }
 
         k_msleep(1000);
     }
