8853bdc0fd977f0266495a7115d992c7577ec689
annie <annemarie.ala@edu.sait.ca>
2025-11-14 15:14:54 -0700

    fix syntax errors

--- app/src/my_state_machine.c
+++ app/src/my_state_machine.c
@@ -1,20 +1,39 @@
 /**
- * @file my_state_machine.h
+ * @file my_state_machine.c
  */
 
- #include <zephyr/smf.h> 
-
+#include <zephyr/smf.h>
 #include "LED.h"
 #include "my_state_machine.h"
 
 /*-------------------------------------------------------
- * Function Prototypes 
+ * Type Definitions
  *---------------------------------------------------------*/
-static void led_on_state_entry(void*o) {
-LED_set(LED0, LED_ON);
+enum led_state_machine_states {
+    LED_ON_STATE, 
+    LED_OFF_STATE
+};
+
+// Struct to keep track of state machine state
+typedef struct {
+    struct smf_ctx ctx; // Context variable used by Zephyr SMF
+    uint16_t count;     // Counter to track time in each state
+} led_state_object_t;
+
+/*-------------------------------------------------------
+ * Local Variables
+ *---------------------------------------------------------*/
+// FIXED: Declare the actual state object variable
+static led_state_object_t led_state_object;
+
+/*-------------------------------------------------------
+ * State Functions
+ *---------------------------------------------------------*/
+static void led_on_state_entry(void *o) {
+    LED_set(LED0, LED_ON);
 }
 
-static enum smf_state_result led_on_state_run(void*o) { 
+static enum smf_state_result led_on_state_run(void *o) { 
     if (led_state_object.count > 500) {
         led_state_object.count = 0;
         smf_set_state(SMF_CTX(&led_state_object), &led_states[LED_OFF_STATE]);
@@ -25,12 +44,13 @@ static enum smf_state_result led_on_state_run(void*o) {
     return SMF_EVENT_HANDLED;
 }
 
-static void led_off_state_entry(void*0){ 
+// FIXED: Changed void*0 to void*o
+static void led_off_state_entry(void *o) { 
     LED_set(LED0, LED_OFF);
 }
 
-static enum smf_state_result led_off_state_run(void*o) {
-        if (led_state_object.count >500) { 
+static enum smf_state_result led_off_state_run(void *o) {
+    if (led_state_object.count > 500) { 
         led_state_object.count = 0; 
         smf_set_state(SMF_CTX(&led_state_object), &led_states[LED_ON_STATE]);
     } else { 
@@ -41,35 +61,22 @@ static enum smf_state_result led_off_state_run(void*o) {
 }
 
 /*-------------------------------------------------------
- * Type Definitions
+ * State Table
  *---------------------------------------------------------*/
-enum led_state_machine_states {
-    LED_ON_STATE, 
-    LED_OFF_STATE
-};
-
-// zepher uses a struct we define to keep track of the state of the 
-// machine... for the LED state machine we are adding a counter to 
-//keep track of when we need to change state
-
-typedef struct {
-    struct smf_ctx ctx; // context variable used by zephyr to track state machine state
-    uint16_t count; // counter to track time in each state
-} led_state_object_t;
-
-/*-------------------------------------------------------
- * Local Variables
- *---------------------------------------------------------*/
+// FIXED: Added semicolon at the end
 static const struct smf_state led_states[] = {
     [LED_ON_STATE] = SMF_CREATE_STATE(led_on_state_entry, led_on_state_run, NULL),
     [LED_OFF_STATE] = SMF_CREATE_STATE(led_off_state_entry, led_off_state_run, NULL),
-}
-void state_machine_init() { 
-    led_state_object.count = 0;// Initialization code for the state machine
+};
+
+/*-------------------------------------------------------
+ * Public Functions
+ *---------------------------------------------------------*/
+void state_machine_init(void) { 
+    led_state_object.count = 0;
     smf_set_initial(SMF_CTX(&led_state_object), &led_states[LED_ON_STATE]);
 }
 
-int state_machine_run() { 
-    // Code to run the state machine
+int state_machine_run(void) { 
     return smf_run_state(SMF_CTX(&led_state_object));
 }
--- app/src/my_state_machine.h
+++ app/src/my_state_machine.h
@@ -2,10 +2,10 @@
  * @file my_state_machine.h
  */
 
- #ifndef MY_STATE_MACHINE_H 
- #define MY_STATE_MACHINE_H
+#ifndef MY_STATE_MACHINE_H 
+#define MY_STATE_MACHINE_H
 
- void state_machine_init(); 
- int state_machine_run(); 
+void state_machine_init(void); 
+int state_machine_run(void); 
 
 #endif // MY_STATE_MACHINE_H
